e4d4e21f112dc8cc54dfb68f9db324ff1d30ae6a
DEEKSHITH S J <124709951+deekshi07@users.noreply.github.com>
2025-08-14 03:39:35 +0000

    data types

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,8 @@
+
+
+//console.alert(3+3) we are using nodejs not browese so it will throws an error
+//numbers => int longint float
+//string =>var let const
+//boolean => true/false
+//Undefine => null
+//object
