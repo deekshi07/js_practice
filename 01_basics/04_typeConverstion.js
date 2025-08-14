dcf1ab1f139fd21306eb3bf4fa1d817011a2cf1b
DEEKSHITH S J <124709951+deekshi07@users.noreply.github.com>
2025-08-14 04:39:15 +0000

    type conversion

---
+++ 01_basics/04_typeConverstion.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)
+//"33" =>33
+//"32djf" =>NaN
+//true => 1  false => 0
+num = 1
+let booleanIsLoggedIn = Boolean(num)
+console.log(booleanIsLoggedIn)
+
+//1 => true 0=>false
+//'' => flse 
+//"deekshith" => true
