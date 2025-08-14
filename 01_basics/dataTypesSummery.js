34cf1136bf9cd4efd92cddc6c75e57ed4ac84a71
DEEKSHITH S J <124709951+deekshi07@users.noreply.github.com>
2025-08-14 05:36:30 +0000

    data types summery

---
+++ 01_basics/dataTypesSummery.js
@@ -0,0 +1,29 @@
+// primitive
+
+// 7 types: String, Numbers, Boolean,null,undefined, Symbol(unique),BigInt
+
+const score = 100
+const scoreaValue = 100.4
+
+const isLoggedIn= false
+const bigNumber = 27328369426969n
+
+
+
+//Non primitive (Refence type)
+
+//Array ,Objects,Functions
+
+const heros =['shaktiman','nagraj','doga']
+const myObj ={
+    name :'deekshith',
+    age: 24
+
+}
+
+const myFunc = function(){
+    console.log("hello word")
+
+}
+
+console.log(typeof myFunc )
