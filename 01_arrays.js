928be6149c5347e0bd59e63356ed5e0f0106b202
DEEKSHITH S J <124709951+deekshi07@users.noreply.github.com>
2025-08-28 06:57:26 +0000

    arrays

---
+++ 01_arrays.js
@@ -0,0 +1,16 @@
+const myArr = [ 0,1,2,3,4,5,6]
+const myHero = [ "Deekshith","rakshith"]
+
+
+const myArr2 = new Array(1,2,3,4,5,6)
+// console.log(myArr)
+// console.log(myArr2)
+
+// array methods
+
+
+myArr.push(9)
+myArr.push(7)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
